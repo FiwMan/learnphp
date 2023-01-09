122846e820bd70b208844c99dd9dd3955b0c9461
FiwMan <107614410+FiwMan@users.noreply.github.com>
2023-01-09 20:20:18 +0700

    Initial commit

---
+++ pages/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>leanrphp</title>
+    <link rel='stylesheet' href='../css/background.css'>
+    <link rel='stylesheet' href='../css/test.css'>
+    <link rel='stylesheet' href='../bootstrap-5.0.2-dist/css/bootstrap.min.css'>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Homepage</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contactus.php">ContactUs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="test.php">test</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</head>
+<body>
+    <div class="container">
+      <div class="text blur">
+        <h1 style="color : white; font-size : 60px; ">เว็บสารพัดจ้าง</h1>
+        <p>ตรงนี้คืออะไรว่ะ</p>
+        <P class="font">ตรงนี้ด้วย</P>
+      </div>
+    </div>
+</body>
+</html>
---
+++ pages/test.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>leanrphp</title>
+    <link rel='stylesheet' href='../css/background.css'>
+    <link rel='stylesheet' href='../css/test.css'>
+    <link rel='stylesheet' href='../bootstrap-5.0.2-dist/css/bootstrap.min.css'>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Homepage</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contactus.php">ContactUs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="test.php">test</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</head>
+<body>
+    <ul>
+        <il<a href="#"></a>my il</a></il>
+        <il<a href="#"></a>my il</a></il>
+        <il<a href="#"></a>my il</a></il>
+        <il<a href="#"></a>my il</a></il>
+    </ul>
+</body>
+</html>
